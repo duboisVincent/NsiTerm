39e7c3db6f894a8e73b6c8afc3c3048186ef6e32
VincentDubois <70756119+duboisVincent@users.noreply.github.com>
2021-09-07 18:39:59 +0200

    Add files via upload

--- MoyenneNsi.py
+++ MoyenneNsi.py
@@ -1,22 +1,28 @@
-liste = [12,15,78,84,15]
+liste = [12,15,78,84,15] 
 
-def MoyenneElement(liste):
+def MoyenneElement(Liste):
     moyenne = 0
+    Len = len(liste)
     for i in liste:
         moyenne += i
+    moyenne = moyenne / Len
     return moyenne
 print("La moyenne de la liste est",MoyenneElement(liste))
 
-def MoyenneIndice(liste):
+def MoyenneIndice(Liste):
     moyenne = 0
+    Len = len(liste)
     long = len(liste)
     for i in range(long):
         moyenne = moyenne + liste[i]
+    moyenne = moyenne / Len
     return moyenne
 print("La moyenne de la liste est",MoyenneIndice(liste))
 
 k = 0
+Len = len(liste)
 while k != sum(liste):
     k = k + 1
     if k == sum(liste):
-        print("La moyenne de la liste est de",k)
+        print("La moyenne de la liste est",k/ len(liste))
+    
